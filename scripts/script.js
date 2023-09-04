609789d352055fe859a7dde1b587542981f4aba6
youngmin <dudals9901@naver.com>
2023-09-04 15:00:11 +0900

    focus / blur에 반응하여 row/col 하이라이트

--- scripts/script.js
+++ scripts/script.js
@@ -34,17 +34,39 @@ exportButton.addEventListener('click', () => {
 
 for (let rowIndex = 1; rowIndex < table.rows.length; rowIndex++) {
     for (let colIndex = 1; colIndex < table.rows[rowIndex].cells.length; colIndex++) {
-        table.rows[rowIndex].cells[colIndex].firstChild.addEventListener("focus", focusOnCell);
-        table.rows[rowIndex].cells[colIndex].firstChild.addEventListener("blur", blurOutCell);
+
+        const cellInput = table.rows[rowIndex].cells[colIndex].firstChild;
+        cellInput.paramRow = rowIndex;
+        cellInput.paramCol = colIndex;
+        cellInput.addEventListener("focus", focusOnCell);
+        cellInput.addEventListener("blur", blurOutCell);
     }
 }
 
 // 내부 cell에 focus 됐을 때 사용
 function focusOnCell(event) {
-    console.log("cursor on");
+    // console.log("cursor on");
+
+    const rowIndex = event.currentTarget.paramRow;
+    const colIndex = event.currentTarget.paramCol;
+
+    table.rows[0].cells[colIndex].style.backgroundColor = "#AED8E6";
+    table.rows[0].cells[colIndex].style.color = "white";
+
+    table.rows[rowIndex].cells[0].style.backgroundColor = "#AED8E6";
+    table.rows[rowIndex].cells[0].style.color = "white";
 }
 
 // 내부 cell에서 (blur)focusOut 됐을 때 사용
 function blurOutCell(event) {
-    console.log("cursor off");
+    // console.log("cursor off");
+
+    const rowIndex = event.currentTarget.paramRow;
+    const colIndex = event.currentTarget.paramCol;
+
+    table.rows[0].cells[colIndex].style.backgroundColor = "#DEDEDE";
+    table.rows[0].cells[colIndex].style.color = "black";
+
+    table.rows[rowIndex].cells[0].style.backgroundColor = "#DEDEDE";
+    table.rows[rowIndex].cells[0].style.color = "black";
 }
